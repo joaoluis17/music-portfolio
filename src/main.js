376701bbe2541540ea35eb88884e89ed74afc1bd
joaoluis17 <jluispprado@hotmail.com>
2025-05-20 16:24:28 -0300

    adicionando o script do adsense

--- src/main.js
+++ src/main.js
@@ -5,3 +5,10 @@ import router from "./router"; // Importa o roteador
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+
+document.head.insertAdjacentHTML("beforeend", `
+    <!-- AdSense code -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5967689884232154"
+     crossorigin="anonymous"></script>
+    <!-- End AdSense code -->
+  `);
